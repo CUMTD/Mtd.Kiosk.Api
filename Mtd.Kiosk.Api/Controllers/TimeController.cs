80216d00549bc0acd745ce5b9d09582bdeea5e46
Matthew Novelli <mattnovelli@gmail.com>
2024-08-28 10:59:35 -0500

    add dark mode controller

---
+++ Mtd.Kiosk.Api/Controllers/TimeController.cs
@@ -0,0 +1,49 @@
+﻿using CoordinateSharp;
+using Microsoft.AspNetCore.Mvc;
+using NodaTime;
+
+
+namespace Mtd.Kiosk.Api.Controllers;
+
+/// <summary>
+/// 
+/// </summary>
+[ApiController]
+[Produces("application/json")]
+[Route("time")]
+public class TimeController : ControllerBase
+{
+	private readonly ILogger<TimeController> _logger;
+
+	/// <summary>
+	/// </summary>
+	/// <param name="logger"></param>
+	public TimeController(ILogger<TimeController> logger)
+	{
+		ArgumentNullException.ThrowIfNull(logger);
+
+		_logger = logger;
+	}
+
+	/// <summary>
+	/// Returns if dark mode should be enabled based on the current time of day.
+	/// </summary>
+	/// <returns>True if dark mode should be enabled.</returns>
+	[HttpGet("dark-mode")]
+	[ProducesResponseType<bool>(StatusCodes.Status200OK)]
+	[ProducesResponseType<ProblemDetails>(StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(StatusCodes.Status404NotFound)]
+	public ActionResult<bool> GetDarkModeStatus()
+	{
+
+		DateTimeZone zone = DateTimeZoneProviders.Tzdb["America/Chicago"];
+		ZonedDateTime zonedDateTime = SystemClock.Instance.GetCurrentInstant().InZone(zone);
+
+		// Convert to UTC
+		DateTime utcDateTime = zonedDateTime.ToDateTimeUtc();
+
+		var c = new Coordinate(40.11560, -88.19520, utcDateTime);
+		var sunUp = c.CelestialInfo.IsSunUp;
+		return Ok(!sunUp);
+	}
+}
