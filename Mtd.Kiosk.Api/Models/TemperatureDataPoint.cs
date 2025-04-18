e6b5f53864653fd9a4ca75bac470a37da0cddc6e
Matthew Novelli <mattnovelli@gmail.com>
2025-04-18 09:25:36 -0500

    implement basic temp monitoring endpoints

---
+++ Mtd.Kiosk.Api/Models/TemperatureDataPoint.cs
@@ -0,0 +1,30 @@
+﻿using Mtd.Kiosk.Core.Entities;
+using System.Text.Json.Serialization;
+
+namespace Mtd.Kiosk.Api.Models;
+
+/// <summary>
+/// For returning temp data to the frontend
+/// </summary>
+/// <param name="temperature"></param>
+public class TemperatureDataPoint(Temperature temperature)
+{
+	/// <summary>
+	/// The temperature in Fahrenheit.
+	/// </summary>
+	[JsonPropertyName("tempFahrenheit")]
+	public byte TemperatureFahrenheit { get; set; } = temperature.TempFahrenheit;
+
+	/// <summary>
+	/// The relative humidity in percentage.
+	/// </summary>
+	[JsonPropertyName("relHumidity")]
+	public byte RelativeHumidity { get; set; } = temperature.RelHumidity;
+
+	/// <summary>
+	/// The timestamp of the temperature reading in milliseconds since Unix epoch.
+	/// </summary>
+	[JsonPropertyName("timestamp")]
+	public long Timestamp { get; set; } = temperature.Timestamp.ToUnixTimeMilliseconds();
+
+}
---
+++ Mtd.Kiosk.Core/Entities/Temperature.cs
@@ -0,0 +1,28 @@
+﻿
+using Mtd.Core.Entities;
+
+namespace Mtd.Kiosk.Core.Entities;
+
+public class Temperature : IEntity
+{
+	public string KioskId { get; set; } = string.Empty;
+
+	public DateTimeOffset Timestamp { get; set; }
+
+	public byte TempFahrenheit { get; set; }
+
+	public byte RelHumidity { get; set; }
+
+	public Temperature(string kioskId, byte tempFahrenheit, byte relHumidity) : this()
+	{
+		KioskId = kioskId;
+		TempFahrenheit = tempFahrenheit;
+		RelHumidity = relHumidity;
+		Timestamp = DateTimeOffset.Now.ToLocalTime();
+	}
+
+	public Temperature()
+	{
+		Timestamp = DateTimeOffset.Now.ToLocalTime();
+	}
+}
